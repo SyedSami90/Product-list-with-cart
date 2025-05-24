8370551901525a1d53a8b52f89bfb13e9bf4abd6
SyedSami90 <syedzaman8989@gmail.com>
2025-05-24 14:28:51 +0500

    design almost completed

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const product_list = document.getElementById('product-list')
 function chooseImg(data){
     const w = window.innerWidth
     if (w >= 1024) return data.image.desktop 
-    else if (w >=600)return data.image.tablet
+    else if (w >=620)return data.image.tablet
     return data.image.mobile
 } 
 
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded',()=>{
             product_list.append(li)
 
             currentImg.classList.add('rounded-lg','my-9') //my-9 was her 
-            currentBtn.classList.add('bg-white','flex','items-center','gap-2','border-[1px]','border-[hsl(12,20%,44%)]','p-3', 'px-9', 'rounded-4xl','absolute','translate-y-[-69px]','cursor-pointer','left-1/2', '-translate-x-1/2','min-w-[200px]','justify-center','hover:border-[hsl(14,86%,42%)]','transition-transform','duration-200','hover:translate-y-[-71px]') //worry about translate-x later
+            currentBtn.classList.add('bg-white','flex','items-center','gap-2','border-[1px]','border-[hsl(12,20%,44%)]','p-3', 'px-9', 'rounded-4xl','absolute','translate-y-[-69px]','cursor-pointer','left-1/2', '-translate-x-1/2','justify-center','hover:border-[hsl(14,86%,42%)]','transition-transform','duration-200','hover:translate-y-[-71px]','whitespace-nowrap') //worry about translate-x later
             li.classList.add('relative')
             //<button class="flex items-center gap-2 border-[1px] p-3 px-9 rounded-4xl">
         }
